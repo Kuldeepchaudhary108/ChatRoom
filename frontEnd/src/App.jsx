70eb4889d4bca054fc8b7fdc0f52c226d20d0e66
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2024-11-21 20:17:49 +0530

    install variable

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "./context/theme";
 
 export default function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
   return (
-    <div>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <Outlet />
-    </div>
+    </ThemeProvider>
   );
 }
